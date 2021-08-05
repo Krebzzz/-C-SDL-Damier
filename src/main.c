98fc58b6c5cbf24cff925ca271bed38ce76568a8
Krebz <81075573+Krebzzz@users.noreply.github.com>
2021-08-05 15:16:49 +0200

    Ajout du main

---
+++ src/main.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL.h>
+ 
+//--------------------------------------
+
+void SDL_ExitWithError(char *message);
+int SDL_DrawColor(SDL_Renderer *renderer, SDL_Color *color);
+//--------------------------------------
+
+int main(int argc, char *argv[])
+{
+
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    SDL_Rect rects[50];
+    SDL_Color blanc = {255, 255, 255, 255};
+//--------------------------------------
+    //Création Damier :
+    size_t i = 0;
+    for(i = 0; i < 50; i++)
+    {   
+        rects[i].w = 50;
+        rects[i].h = 50;
+        rects[i].x = 100 * (i % 5) + 50 * ((i / 5) % 2);
+        rects[i].y = 50 * (i / 5);
+    }
+//--------------------------------------
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        SDL_ExitWithError("Initialisation SDL");
+    }
+    
+    if (SDL_CreateWindowAndRenderer( 500, 500, 0, &window, &renderer) != 0)
+    {
+        SDL_ExitWithError("Impossible de crée la fenêtre et du rendu, veuillez réessayer");
+    }
+    if (SDL_DrawColor(renderer,&blanc ) != 0)
+    {
+        SDL_ExitWithError("Impossible de crée la couleur");
+    }
+
+    if (SDL_RenderFillRects(renderer, rects , 50) != 0)
+    {
+        SDL_ExitWithError("Erreur Création Damier");
+    }
+    
+    SDL_SetWindowTitle(window, "Damier");
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(6000);
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit(); 
+    return EXIT_SUCCESS; 
+}
+
+int SDL_DrawColor(SDL_Renderer *renderer, SDL_Color *color){
+    if(0 != SDL_SetRenderDrawColor(renderer, color->r, color->g, color->b, color->a))
+    {
+        return -1;
+    }
+    return 0;
+}
+
+void SDL_ExitWithError(char *message){
+    SDL_Log("ERREUR : %s > %s", message, SDL_GetError());
+    SDL_Quit();
+    exit(EXIT_FAILURE);
+}
